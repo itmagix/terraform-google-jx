b91556b76595ddc6af9f60c888fe72d0c845c35b
James Rawlings <rawlingsj80@gmail.com>
2020-09-02 14:25:32 +0100

    Update main.tf

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -114,9 +114,7 @@ resource "kubernetes_config_map" "jenkins_x_requirements" {
   }
 
   lifecycle {
-    ignore_changes = [
-      all
-    ]
+    ignore_changes = all
   }
   depends_on = [
     google_container_cluster.jx_cluster
